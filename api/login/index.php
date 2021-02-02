71f3b43c46afb4b6c8718ee221517882e4161d0a
shiun <pu758614@gmail.com>
2021-02-03 02:23:59 +0800

    [login]登入取得權杖

---
+++ api/login/index.php
@@ -0,0 +1,57 @@
+<?php
+include_once(dirname(__DIR__)."/lib/common.php");
+include_once("$ROOT_PATH/api/lib/db_lib.php");
+$request_data = array(
+    'error' => true,
+    'msg' => '',
+);
+$db = new db_lib();
+$response_data = file_get_contents("php://input");
+$response_arr = json_decode($response_data,'true');
+if(!is_array($response_arr)){
+    $request_data['msg'] = '資料解析失敗';
+    goto end;
+}
+
+$check_data = check_sort_data($response_arr);
+if($check_data['error']){
+    $request_data['msg'] = $check_data['msg'];
+    goto end;
+}
+
+
+
+$data = isset($check_data['data'])?$check_data['data']:'';
+$token_msg = $data['login_name']."_".date('Y-m-d H:i:s',strtotime("+$TOKEN_TIME second"));
+$token = getEncodeStr($token_msg);
+$request_data['data'] = array(
+    'token' => $token
+);
+
+
+end:
+    echo json_encode($request_data);
+
+function check_sort_data($data){
+
+    $return = array(
+        'error' => true,
+        'msg' => '',
+        'data' => array(),
+    );
+
+    if(!isset($data['login_name'])){
+        $return['msg'] = '缺少login_name';
+        return $return;
+    }
+
+    if(!$data['login_name']=='kdan'){
+        $return['msg'] = '錯誤的login_name';
+        return $return;
+    }
+    $return['error'] = false;
+    $return['data'] = array(
+        'login_name' => $data['login_name'],
+    );
+    return $return;
+}
